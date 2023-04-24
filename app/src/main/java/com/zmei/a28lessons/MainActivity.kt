58f1c6f0397b805d48f7cf584d3fc810167957de
Arhizmei <arhizmei@gmail.com>
2023-04-24 21:23:56 +0300

    home2

--- app/src/main/java/com/zmei/a28lessons/MainActivity.kt
+++ app/src/main/java/com/zmei/a28lessons/MainActivity.kt
@@ -2,10 +2,17 @@ package com.zmei.a28lessons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+        return true
     }
 }
